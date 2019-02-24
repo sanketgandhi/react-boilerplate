9cc9aaa4ce338a0383b4232043531b90c9827db3
sanketgandhi <sanketgandhi876@gmail.com>
2019-02-24 18:15:07 +0530

    Clean up

--- src/App.js
+++ src/App.js
@@ -1,42 +1,11 @@
 import React from 'react';
 import { hot } from 'react-hot-loader';
 
-// Import modules asynchronously.
-const Warning = React.lazy(() => import('./Warning'));
-
 class App extends React.Component {
-    state = {
-        count: 0
-    };
     render() {
-        const { count } = this.state;
         return (
             <div>
                 <h1>Hello World!!!</h1>
-                <h2 className={count > 10 ? 'warning' : null}>
-                    count: {count}
-                </h2>
-                <button
-                    onClick={() =>
-                        this.setState(state => ({ count: state.count + 1 }))
-                    }
-                >
-                    {' '}
-                    +{' '}
-                </button>
-                <button
-                    onClick={() =>
-                        this.setState(state => ({ count: state.count - 1 }))
-                    }
-                >
-                    {' '}
-                    -{' '}
-                </button>
-                {count > 10 ? (
-                    <React.Suspense fallback={null}>
-                        <Warning />
-                    </React.Suspense>
-                ) : null}
             </div>
         );
     }
